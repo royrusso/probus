962057d8571883e09096ce2d3ac22417b2898d8d
Roy Russo <royrusso@gmail.com>
2024-10-31 10:59:06 -0400

    --

--- backend/test_main.py
+++ backend/test_main.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_default():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Net Pretzel says": "Hello World"}  
