e3e552c7c588fa4f5384e84709847e364dde5aad
Roy Russo <royrusso@gmail.com>
2024-10-31 11:29:29 -0400

    --

--- backend/test_main.py
+++ backend/test_main.py
@@ -8,3 +8,8 @@ def test_read_default():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Net Pretzel says": "Hello World"}  
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
